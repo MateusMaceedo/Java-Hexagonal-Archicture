f0b215608df23e72b3264d44bf92527746eb6a65
Mateus Macedo <mateusouza2014@live.com>
2023-06-26 20:17:41 -0300

    feat: Adicionando teste unitarios do usecase. BuscarProdutosUseCaseImplTest

--- src/test/java/com/example/springboot/domain/usecases/impl/BuscarProdutosUseCaseImplTest.java
+++ src/test/java/com/example/springboot/domain/usecases/impl/BuscarProdutosUseCaseImplTest.java
@@ -1,5 +1,54 @@
 package com.example.springboot.domain.usecases.impl;
 
+import com.example.springboot.domain.dto.requests.produtos.ProdutoDTO;
+import com.example.springboot.domain.exceptions.ProductNotFoundException;
+import com.example.springboot.domain.mapper.Produto;
+import com.example.springboot.domain.ports.interfaces.BuscarProdutosInterface;
+import com.example.springboot.infrastructure.config.repositories.ProdutoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 public class BuscarProdutosUseCaseImplTest {
 
+    @Mock
+    private ProdutoRepository produtoRepository;
+
+    private BuscarProdutosInterface buscarProdutosUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        buscarProdutosUseCase = new BuscarProdutosUseCaseImpl(produtoRepository);
+    }
+
+    @Test
+    void buscarProdutos_deveRetornarListaDeProdutosDTO() {
+        List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto());
+        produtos.add(new Produto());
+
+        when(produtoRepository.buscarTodos()).thenReturn(produtos);
+
+        List<ProdutoDTO> produtosDTO = buscarProdutosUseCase.buscarProdutos();
+
+        assertEquals(2, produtosDTO.size());
+    }
+
+    @Test
+    void buscarProdutos_deveLancarProductNotFoundException_quandoListaDeProdutosVazia() {
+        when(produtoRepository.buscarTodos()).thenReturn(new ArrayList<>());
+
+        assertThrows(ProductNotFoundException.class, () -> {
+            buscarProdutosUseCase.buscarProdutos();
+        });
+    }
 }
