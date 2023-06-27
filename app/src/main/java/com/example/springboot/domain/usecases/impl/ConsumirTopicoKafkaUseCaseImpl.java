8d5989f750e1c47db81e694da326ea25e1d0d0b5
Mateus Macedo <mateusouza2014@live.com>
2023-06-26 23:45:03 -0300

    feat: Adicionando configuração da implementação kafka

---
+++ app/src/main/java/com/example/springboot/domain/usecases/impl/ConsumirTopicoKafkaUseCaseImpl.java
@@ -0,0 +1,6 @@
+package com.example.springboot.domain.usecases.impl;
+
+import com.example.springboot.domain.ports.interfaces.ConsumirTopicoKafkaInterface;
+
+public class ConsumirTopicoKafkaUseCaseImpl implements ConsumirTopicoKafkaInterface {
+}
